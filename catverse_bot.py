37d24376d59f5235418df803247aec168e111ff3
abcdefghij803 <analtamash1@gmail.com>
2026-01-27 10:11:03 +0530

    Update catverse_bot.py

--- catverse_bot.py
+++ catverse_bot.py
@@ -807,6 +807,7 @@ def main():
     app.add_handler(CommandHandler("protect", protect))
     app.add_handler(CommandHandler("toprich", toprich))
     app.add_handler(CommandHandler("topkill", topkill))
+    app.add_handler(CallbackQueryHandler(leaderboard_callback, pattern="^lb_"))
     app.add_handler(CommandHandler("shop", shop))
     app.add_handler(CommandHandler("inventory", inventory))
     app.add_handler(CommandHandler("fun", fun))
