18fd7a816b3815778ab908a5c0adec7d9a21408c
meowtasticbot <moonop615@gmail.com>
2026-02-28 00:40:12 +0530

    Update catverse_bot.py

--- catverse_bot.py
+++ catverse_bot.py
@@ -110,7 +110,7 @@ def calculate_global_rank(user_id: int) -> int:
     return 0
 
 
-from Meowstric.plugins.admin import admin_commands, plp, welcome_new_member, welcome_new_members_messag
+from Meowstric.plugins.admin import admin_commands, plp, welcome_new_member, welcome_new_members_message
 from Meowstric.plugins.broadcast import gbroadcast, ubroadcast
 from Meowstric.plugins.chatbot import chat_handler
 from Meowstric.plugins.economy import bal, claim, daily, gift, give, inventory, rob, use
