17e3f96e762cf71518974d2a784ac3d7951656e3
meowtasticbot <moonop615@gmail.com>
2026-02-28 01:29:47 +0530

    Update config.py

--- Meowstric/config.py
+++ Meowstric/config.py
@@ -16,7 +16,7 @@ if not MONGO_URI:
 
 GROQ_API_KEY = os.getenv("GROQ_API_KEY")
 CHATBOT_BOT_TOKEN = os.getenv("CHATBOT_BOT_TOKEN", BOT_TOKEN)
-START_IMG_URL = os.getenv("START_IMG_URL", "https://telegra.ph/file/7f5e8b6c2cf0f2ad9f7d5.jpg")
+START_IMG_URL = os.getenv("START_IMG_URL", "https://img.sanishtech.com/u/7a53054460bf7f0318de8cb3e838412a.png")
 OWNER_LINK = os.getenv("OWNER_LINK", "https://t.me/btw_moon")
 
 # Sudo ids from env, comma separated
@@ -24,8 +24,8 @@ _raw_sudo_ids = os.getenv("SUDO_IDS", "")
 SUDO_IDS = [int(x.strip()) for x in _raw_sudo_ids.split(",") if x.strip().isdigit()]
 MISTRAL_API_KEY = os.getenv("MISTRAL_API_KEY")
 CODESTRAL_API_KEY = os.getenv("CODESTRAL_API_KEY")
-WELCOME_IMG_URL = os.getenv("WELCOME_IMG_URL", "https://telegra.ph/file/7f5e8b6c2cf0f2ad9f7d5.jpg")
-SUPPORT_GROUP = os.getenv("SUPPORT_GROUP", "https://t.me/+hvxrr2DudTs4ODU1")
+WELCOME_IMG_URL = os.getenv("WELCOME_IMG_URL", "https://img.sanishtech.com/u/7a53054460bf7f0318de8cb3e838412a.png")
+SUPPORT_GROUP = os.getenv("SUPPORT_GROUP", "https://t.me/meowstric")
 DIVORCE_COST = int(os.getenv("DIVORCE_COST", "5000"))
 START_TIME = int(os.getenv("START_TIME", str(int(time.time()))))
 WAIFU_PROPOSE_COST = int(os.getenv("WAIFU_PROPOSE_COST", "1500"))
